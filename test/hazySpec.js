f98c95882c4c31b11ef643d161cda31de3b8162f
slurmulon <anon@blaqkmail.com>
2015-05-31 20:30:28 -0700

    :trollface: jk mocha

--- test/hazySpec.js
+++ test/hazySpec.js
@@ -1,43 +1,103 @@
-var hazy = require('../src/hazy')
+var hazy   = require('../src/hazy'),
+    assert = require('assert')
 
-exports.lang = function(test) {
-    test.expect(1);
-    test.ok(true, "this assertion should pass");
-    test.done();
+describe('lang', function() {
+  describe('expressions', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
 
-    // expressions
+  describe('tokens', function() {
+    describe('|', function() {
 
-    // |
-    // :
-    // ~
-    // @
-    // *
+    })
 
-    // process
-}
+    describe(':', function() {
+      
+    })
 
-exports.fixture = function(test) {
-    test.expect(1);
-    test.ok(true, "this assertion should pass");
-    test.done();
+    describe('~', function() {
+      
+    })
 
-    // get
-    // all
-    // register
-    // process
-    // query
-    // load
-}
+    describe('@', function() {
+      
+    })
 
-exports.matcher = function(test) {
-    test.expect(1);
-    test.ok(true, "this assertion should pass");
-    test.done();
+    describe('*', function() {
+      
+    })
+  })
 
-    // config
-    // matches
-    // hasMatch
-    // search
-    // process
-    // processDeep
-}
+  describe('process()', function() {
+
+  })
+})
+
+describe('fixture', function() {
+  describe('expressions', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+
+  describe('get()', function() {
+
+  })
+
+  describe('all()', function() {
+    
+  })
+
+  describe('register()', function() {
+    
+  })
+
+  describe('process()', function() {
+    
+  })
+
+  describe('query()', function() {
+    
+  })
+
+  describe('load()', function() {
+    
+  })
+})
+
+describe('matcher', function() {
+  describe('expressions', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+
+  describe('config()', function() {
+
+  })
+
+  describe('matches()', function() {
+    
+  })
+
+  describe('hasMatch()', function() {
+    
+  })
+
+  describe('search()', function() {
+    
+  })
+
+  describe('process()', function() {
+    
+  })
+
+  describe('processDeep()', function() {
+    
+  })
+})
